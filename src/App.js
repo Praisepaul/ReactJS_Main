7bdc83704b24ea059171b696f4d7cbffa1953484
PsychotherapyCentral <psychotherapy.first@gmail.com>
2023-10-14 11:27:35 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
-import GitHubLinks from './src/components/Githublinks.jsx';
-import SiteLinks from './src/components/sitelinks.jsx';
 
 function App() {
   return (
     <div className="App">
       <h1>Project Landing Page</h1>
       <div className="button-container">
-        <GitHubLinks />
+      <div className="links">
+      <div className="links">
+      <h2>GitHub Repositories</h2>
+      <ul>
+      <li className="li"><a href="https://github.com/Praisepaul/age_calculator" target="_blank" rel="noopener noreferrer">Task 1 - Age Calculator</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/api_fetcher" target="_blank" rel="noopener noreferrer">Task 2 - API Data Fetcher</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/calculator_App" target="_blank" rel="noopener noreferrer">Task 3 - Calculator App</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/color_picker_App" target="_blank" rel="noopener noreferrer">Task 4 - Color Picker</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/Paragraph_Counter" target="_blank" rel="noopener noreferrer">Task 5 - Paragraph Counter</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/sample_portfolio" target="_blank" rel="noopener noreferrer">Task 6 - Personal Portfolio</a></li>
+      </ul>
+    </div>
+      <h2>Working Sites</h2>
+      <ul className='li'>
+      <li className="li"><a href="https://praisepaul.github.io/age_calculator/" target="_blank" rel="noopener noreferrer">Task 1 - Age Calculator</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/api_fetcher/" target="_blank" rel="noopener noreferrer">Task 2 - API Data Fetcher</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/calculator_App/" target="_blank" rel="noopener noreferrer">Task 3 - Calculator App</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/color_picker_App/" target="_blank" rel="noopener noreferrer">Task 4 - Color Picker</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/Paragraph_Counter/" target="_blank" rel="noopener noreferrer">Task 5 - Paragraph Counter</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/sample_portfolio/" target="_blank" rel="noopener noreferrer">Task 6 - Personal Portfolio</a></li>
+    </ul>
+    </div>
         <SiteLinks />
       </div>
     </div>
