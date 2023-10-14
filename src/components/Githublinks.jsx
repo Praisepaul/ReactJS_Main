3a213084992a114e15e8a30fba5f29e4971a25c1
PsychotherapyCentral <psychotherapy.first@gmail.com>
2023-10-14 11:38:30 +0530

    Some changes

---
+++ src/components/Githublinks.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Githublinks = () => {
+  return (
+    <div className="links">
+      <h2>GitHub Repositories</h2>
+      <ul>
+      <li className="li"><a href="https://github.com/Praisepaul/age_calculator" target="_blank" rel="noopener noreferrer">Task 1 - Age Calculator</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/api_fetcher" target="_blank" rel="noopener noreferrer">Task 2 - API Data Fetcher</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/calculator_App" target="_blank" rel="noopener noreferrer">Task 3 - Calculator App</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/color_picker_App" target="_blank" rel="noopener noreferrer">Task 4 - Color Picker</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/Paragraph_Counter" target="_blank" rel="noopener noreferrer">Task 5 - Paragraph Counter</a></li>
+      <li className="li"><a href="https://github.com/Praisepaul/sample_portfolio" target="_blank" rel="noopener noreferrer">Task 6 - Personal Portfolio</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Githublinks;
---
+++ src/components/sitelinks.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SiteLinks = () => {
+  return (
+    <div className="links">
+      <h2>Working Sites</h2>
+      <ul className='li'>
+      <li className="li"><a href="https://praisepaul.github.io/age_calculator/" target="_blank" rel="noopener noreferrer">Task 1 - Age Calculator</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/api_fetcher/" target="_blank" rel="noopener noreferrer">Task 2 - API Data Fetcher</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/calculator_App/" target="_blank" rel="noopener noreferrer">Task 3 - Calculator App</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/color_picker_App/" target="_blank" rel="noopener noreferrer">Task 4 - Color Picker</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/Paragraph_Counter/" target="_blank" rel="noopener noreferrer">Task 5 - Paragraph Counter</a></li>
+      <li className="li"><a href="https://praisepaul.github.io/sample_portfolio/" target="_blank" rel="noopener noreferrer">Task 6 - Personal Portfolio</a></li>
+    </ul>
+    </div>
+  );
+}
+
+export default SiteLinks;
