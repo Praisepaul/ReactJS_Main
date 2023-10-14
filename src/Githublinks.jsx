4bd62d69c3132c71554bf5473c562f8cee8d8062
PsychotherapyCentral <psychotherapy.first@gmail.com>
2023-10-14 13:15:04 +0530

    Deployed

--- src/Githublinks.jsx
+++ src/Githublinks.jsx
@@ -4,72 +4,124 @@ const Githublinks = () => {
   return (
     <div className="links-container">
       <h2>GitHub Repositories</h2>
-      <ul>
-        <li>
-          <a href="https://github.com/Praisepaul/age_calculator" target="_blank" rel="noopener noreferrer">
-            Task 1 - Age Calculator
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Praisepaul/api_fetcher" target="_blank" rel="noopener noreferrer">
-            Task 2 - API Data Fetcher
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Praisepaul/calculator_App" target="_blank" rel="noopener noreferrer">
-            Task 3 - Calculator App
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Praisepaul/color_picker_App" target="_blank" rel="noopener noreferrer">
-            Task 4 - Color Picker
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Praisepaul/Paragraph_Counter" target="_blank" rel="noopener noreferrer">
-            Task 5 - Paragraph Counter
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Praisepaul/sample_portfolio" target="_blank" rel="noopener noreferrer">
-            Task 6 - Personal Portfolio
-          </a>
-        </li>
-      </ul>
+      <table className="links-table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Repository</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Task 1 - Age Calculator</td>
+            <td>
+              <a href="https://github.com/Praisepaul/age_calculator" target="_blank" rel="noopener noreferrer">
+                GitHub Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 2 - API Data Fetcher</td>
+            <td>
+              <a href="https://github.com/Praisepaul/api_fetcher" target="_blank" rel="noopener noreferrer">
+                GitHub Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 3 - Calculator App</td>
+            <td>
+              <a href="https://github.com/Praisepaul/calculator_App" target="_blank" rel="noopener noreferrer">
+                GitHub Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 4 - Color Picker</td>
+            <td>
+              <a href="https://github.com/Praisepaul/color_picker_App" target="_blank" rel="noopener noreferrer">
+                GitHub Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 5 - Paragraph Counter</td>
+            <td>
+              <a href="https://github.com/Praisepaul/Paragraph_Counter" target="_blank" rel="noopener noreferrer">
+                GitHub Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 6 - Personal Portfolio</td>
+            <td>
+              <a href="https://github.com/Praisepaul/sample_portfolio" target="_blank" rel="noopener noreferrer">
+                GitHub Link
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
       <h2>Working Sites</h2>
-      <ul>
-        <li>
-          <a href="https://praisepaul.github.io/age_calculator/" target="_blank" rel="noopener noreferrer">
-            Task 1 - Age Calculator
-          </a>
-        </li>
-        <li>
-          <a href="https://praisepaul.github.io/api_fetcher/" target="_blank" rel="noopener noreferrer">
-            Task 2 - API Data Fetcher
-          </a>
-        </li>
-        <li>
-          <a href="https://praisepaul.github.io/calculator_App/" target="_blank" rel="noopener noreferrer">
-            Task 3 - Calculator App
-          </a>
-        </li>
-        <li>
-          <a href="https://praisepaul.github.io/color_picker_App/" target="_blank" rel="noopener noreferrer">
-            Task 4 - Color Picker
-          </a>
-        </li>
-        <li>
-          <a href="https://praisepaul.github.io/Paragraph_Counter/" target="_blank" rel="noopener noreferrer">
-            Task 5 - Paragraph Counter
-          </a>
-        </li>
-        <li>
-          <a href="https://praisepaul.github.io/sample_portfolio/" target="_blank" rel="noopener noreferrer">
-            Task 6 - Personal Portfolio
-          </a>
-        </li>
-      </ul>
+      <table className="links-table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Site</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Task 1 - Age Calculator</td>
+            <td>
+              <a href="https://praisepaul.github.io/age_calculator" target="_blank" rel="noopener noreferrer">
+                Site Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 2 - API Data Fetcher</td>
+            <td>
+              <a href="https://praisepaul.github.io/api_fetcher" target="_blank" rel="noopener noreferrer">
+                Site Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 3 - Calculator App</td>
+            <td>
+              <a href="https://praisepaul.github.io/calculator_App" target="_blank" rel="noopener noreferrer">
+                Site Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 4 - Color Picker</td>
+            <td>
+              <a href="https://praisepaul.github.io/color_picker_App" target="_blank" rel="noopener noreferrer">
+                Site Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 5 - Paragraph Counter</td>
+            <td>
+              <a href="https://praisepaul.github.io/Paragraph_Counter" target="_blank" rel="noopener noreferrer">
+                Site Link
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Task 6 - Personal Portfolio</td>
+            <td>
+              <a href="https://praisepaul.github.io/sample_portfolio" target="_blank" rel="noopener noreferrer">
+                Site Link
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
